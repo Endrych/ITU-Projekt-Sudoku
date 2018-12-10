acfc2c921568fee1fbca8a898c5a1d5869d2f263
Your Name <kukin1996@seznam.cz>
2018-12-10 03:12:16 +0100

    Pridana stredni a lehka obtiznost

--- src/main/helpers/fields.js
+++ src/main/helpers/fields.js
@@ -10,32 +10,164 @@ export default {
             [[5, null, null], [6, null, null], [null, null, 7]],
             [[null, null, null], [null, null, 4], [1, 5, null]],
             [[null, null, 3], [9, null, 8], [null, 2, 4]]
+		],
+		[
+            [[null, 7, null], [null, 1, 2], [5, null, 9]],
+            [[null, 9, 1], [null,null,null], [8, 3, null]],
+            [[4, null, null], [null, 3, null], [1, null, 2]],
+            [[null, null, null], [5, 8, 1], [null, null, null]],
+            [[null, 1, null], [null, 9, null], [null, 2, null]],
+            [[null, null, null], [3, 2, 4], [null, null, null]],
+            [[7, null, 4], [null, 6, null], [null, null, 3]],
+            [[null, 2, 6], [null, null, null], [7, 8, null]],
+            [[8, null, 9], [1, 4, null], [null, 6, null]]
+        ],
+		[
+            [[8,9,5], [null,4,null], [7,null,null]],
+            [[null,null,7], [9,null,null], [1,null,4]],
+            [[4,null,null], [null,8,null], [null,null,5]],
+            [[null,null,null], [null,null,null], [2,3,8]],
+            [[null,1,2], [null,6,null], [5,7,null]],
+            [[7,8,9], [null,null,null], [null,null,null]],
+            [[6,null,null], [null,9,null], [null,null,1]],
+            [[1,null,4], [null,null,5], [6,null,null]],
+            [[null,null,8], [null,1,null], [3,5,7]]
+        ],
+		[
+            [[null,3,5], [null,4,null], [1,null,6]],
+            [[null,1,null], [2,null,null], [7,null,null]],
+            [[null,null,6], [null,null,8], [3,null,4]],
+            [[6,null,null], [4,1,null], [null,5,null]],
+            [[null,null,null], [8,null,3], [null,null,null]],
+            [[null,9,null], [null,2,5], [null,null,7]],
+            [[2,null,9], [3,null,null], [5,null,null]],
+            [[null,null,7], [null,null,4], [null,6,null]],
+            [[5,null,1], [null,8,null], [9,4,null]]
+        ],
+		[
+            [[6,null,null], [7,null,null], [null,4,8]],
+            [[7,null,null], [5,null,null], [6,null,null]],
+            [[null,null,null], [null,3,null], [1,5,null]],
+            [[null,null,7], [8,5,null], [null,9,null]],
+            [[5,9,null], [null,7,null], [null,1,3]],
+            [[null,3,null], [null,1,6], [5,null,null]],
+            [[null,6,9], [null,8,null], [null,null,null]],
+            [[null,null,4], [null,null,5], [null,null,1]],
+            [[3,2,null], [null,null,4], [null,null,9]]
         ]
     ],
     standard: [
-        [
-            [[4, 5, null], [2, null, 8], [3, null, null]],
-            [[null, 8, 2], [3, null, null], [null, null, null]],
-            [[7, null, null], [null, null, 6], [null, null, 1]],
-            [[null, null, null], [null, null, null], [8, null, null]],
-            [[null, 3, null], [8, 1, 9], [null, 4, null]],
-            [[null, null, 2], [null, null, null], [null, null, null]],
-            [[5, null, null], [6, null, null], [null, null, 7]],
-            [[null, null, null], [null, null, 4], [1, 5, null]],
-            [[null, null, 3], [9, null, 8], [null, 2, 4]]
+		[
+            [[null, 8, null], [9, null, null], [null, null, 7]],
+            [[null, null, 4], [null,null,null], [8,2,null]],
+            [[3,null,null], [8,null,null], [4,9,6]],
+            [[1,null,null], [null,null,null], [null,7,5]],
+            [[null,null,9], [null,7,null], [6,null,null]],
+            [[7,6,null], [null,null,null], [null,null,9]],
+            [[4,2,7], [null,null,1], [null,null,8]],
+            [[null,9,1], [null,null,null], [3,null,null]],
+            [[6,null,null], [null,null,5], [null,1,null]]
+        ],
+		[
+            [[7,null,1], [null,null,4], [null,3,5]],
+            [[null,null,null], [null,null,1], [null,null,9]],
+            [[null,6,5], [null,3,null], [null,null,null]],
+            [[1,null,null], [null,null,null], [null,9,null]],
+            [[null,null,8], [2,null,6], [1,null,null]],
+            [[null,2,null], [null,null,null], [null,null,4]],
+            [[null,null,null], [null,6,null], [3,5,null]],
+            [[3,null,null], [1,null,null], [null,null,null]],
+            [[9,5,null], [3,null,null], [2,null,7]]
+        ],
+		[
+            [[null,null,8], [null,null,null], [null,4,3]],
+            [[null,null,7], [null,8,3], [null,null,2]],
+            [[null,null,null], [4,null,6], [null,9,null]],
+            [[6,3,null], [2,null,null], [null,7,9]],
+            [[null,null,null], [1,null,8], [null,null,null]],
+            [[9,8,null], [null,null,7], [null,1,4]],
+            [[null,2,null], [5,null,4], [null,null,null]],
+            [[8,null,null], [6,2,null], [4,null,null]],
+            [[1,4,null], [null,null,null], [2,null,null]]
+        ],
+		[
+            [[null,null,null], [null,null,null], [null,null,null]],
+            [[null,2,1], [null,8,null], [null,3,4]],
+            [[6,4,8], [null,1,null], [null,null,9]],
+            [[null,null,null], [null,9,7], [null,4,8]],
+            [[null,null,null], [1,null,3], [null,null,null]],
+            [[7,3,null], [6,2,null], [null,null,null]],
+            [[3,null,null], [null,6,null], [4,1,7]],
+            [[4,6,null], [null,7,null], [2,8,null]],
+            [[null,null,null], [null,null,null], [null,null,null]]
+        ],
+		[
+            [[null,5,2], [null,null,null], [8,null,null]],
+            [[4,null,1], [null,null,8], [null,null,9]],
+            [[null,8,null], [null,null,4], [null,null,5]],
+            [[null,null,7], [null,9,6], [null,null,2]],
+            [[null,null,null], [null,7,null], [null,null,null]],
+            [[2,null,null], [3,8,null], [7,null,null]],
+            [[7,null,null], [2,null,null], [null,6,null]],
+            [[6,null,null], [8,null,null], [5,null,4]],
+            [[null,null,3], [null,null,null], [1,8,null]]
         ]
     ],
     hard: [
-        [
-            [[4, 5, null], [2, null, 8], [3, null, null]],
-            [[null, 8, 2], [3, null, null], [null, null, null]],
-            [[7, null, null], [null, null, 6], [null, null, 1]],
-            [[null, null, null], [null, null, null], [8, null, null]],
-            [[null, 3, null], [8, 1, 9], [null, 4, null]],
-            [[null, null, 2], [null, null, null], [null, null, null]],
-            [[5, null, null], [6, null, null], [null, null, 7]],
-            [[null, null, null], [null, null, 4], [1, 5, null]],
-            [[null, null, 3], [9, null, 8], [null, 2, 4]]
+		[
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []]
+        ],
+		[
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []]
+        ],
+		[
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []]
+        ],
+		[
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []]
+        ],
+		[
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []],
+            [[], [], []]
         ]
     ]
 };
